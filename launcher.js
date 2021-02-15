65ab450e6a99ba31313f61c03f821ec333092880
Huangang Zheng <2432036+hzheng1@users.noreply.github.com>
2021-02-15 01:00:40 -0700

    Create launcher.js

---
+++ launcher.js
@@ -0,0 +1,7 @@
+const script = document.createElement('script');
+document.body.appendChild(script);
+script.async = true;
+script.src = 'https://cdn.jsdelivr.net/gh/hzheng1/sealedplay/bookmarklet.js';
+
+
+// javascript:(function()%7Bconst%20script%20%3D%20document.createElement('script')%3Bdocument.body.appendChild(script)%3Bscript.async%20%3D%20true%3Bscript.src%20%3D%20'https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2Fhzheng1%2Fsealedplay%2Fbookmarklet.js'%7D)()
